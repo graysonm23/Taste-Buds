c17fe5adf458b4a026f44f54b1f40cb514c7470b
Edward Martey <martey8709@gmail.com>
2019-10-21 22:23:59 -0500

    Search Page dynamically created using jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,17 @@
 //--------------------------------Welcome page------------------------------------
 let welcomePage = $(".welcome-page");
-$("#openingBtn").on("click", function() {
+
+$(searchPage).hide();
+$('#searchBtn').hide();
+
+$("#openingBtn").on("click", function () {
   $(welcomePage).hide();
+  $(searchPage).show();
+  $('#searchBtn').show();
+  $('input')[0].reset();
 });
 //--------------------------------Welcome page------------------------------------
+
+//--------------------------------Search  page------------------------------------
+
+var searchPage = $('.search-page')
