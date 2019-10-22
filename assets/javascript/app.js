bf894938e39752ab6e7f9f1472d3968990a86fd0
EmirVelazquez <emirleovelazquez@gmail.com>
2019-10-22 17:40:24 -0500

    added logic for video card to display after welcome page button is clicked by user

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,22 @@
-//--------------------------------Welcome page------------------------------------
+//--------------------------------Welcome page------------------------------------//
 let welcomePage = $(".welcome-page");
-$("#openingBtn").on("click", function() {
+$("#openingBtn").on("click", function () {
   $(welcomePage).hide();
 });
-//--------------------------------Welcome page------------------------------------
+//--------------------------------Welcome page------------------------------------//
+
+//--------------------------------How-To Video------------------------------------//
+
+/*This on click event will handle when the video card will show, for now it is
+set to openingBtn user click*/
+$("#openingBtn").on("click", function () {
+  //This line will show the howToContainer from html
+  $("#howToContainer").show();
+});
+
+var youTubeApi = AIzaSyA3LJNRXIx7_MkgahxD09FjInN0RrGgsiU;
+var youTubeUrl = "https://www.googleapis.com/youtube/v3/videos"
+
+
+
+//--------------------------------How-To Video------------------------------------//
