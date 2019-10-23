62fab39eaeaf2110de40d999980e9d08e1a29bbd
Grayson M <grayson.mcmurry23@gmail.com>
2019-10-22 20:34:43 -0500

    finished revisions of Accident

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,50 +12,50 @@ $(openingBtn).on("click", function() {
 var searchPage = $(".search-page");
 var searchBtn = $("#searchBtn");
 
-$('#searchBtn').hide();
+$("#searchBtn").hide();
 $(searchPage).hide();
 
-$("#openingBtn").on("click", function () {
+$("#openingBtn").on("click", function() {
   $(searchPage).show();
   $(searchBtn).show();
-
 });
-
-
 
 //--------------------------------Search page------------------------------------//
 
-
 var userFoodSearch = "how to cook chicken marsala";
 var youTubeApi = "AIzaSyA3LJNRXIx7_MkgahxD09FjInN0RrGgsiU";
-var youTubeUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=rating&q=" + userFoodSearch + "&regionCode=us&relevanceLanguage=en&safeSearch=strict&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=any&videoEmbeddable=true&videoLicense=youtube&videoSyndicated=true&videoType=any&key=" + youTubeApi;
+var youTubeUrl =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=rating&q=" +
+  userFoodSearch +
+  "&regionCode=us&relevanceLanguage=en&safeSearch=strict&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=any&videoEmbeddable=true&videoLicense=youtube&videoSyndicated=true&videoType=any&key=" +
+  youTubeApi;
 $.ajax({
   url: youTubeUrl,
   method: "GET"
-}).then(function (response) {
+}).then(function(response) {
   console.log(response);
 });
 
 //This is for the embedded video
 // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
+  player = new YT.Player("player", {
+    height: "390",
+    width: "640",
     //This id was temporarily plugged in from the response
     videoId: "wOZFqf0ojkQ",
     events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
     }
   });
 }
@@ -79,23 +79,25 @@ function stopVideo() {
   player.stopVideo();
 }
 //--------------------------------How-To Video------------------------------------//
-//need to add  $("#recipeList").hide(); in line 12 
-$("#openingBtn").on("click", function () {
+//need to add  $("#recipeList").hide(); in line 12
+$("#openingBtn").on("click", function() {
   //This line will show the recipelist div from html
   $("#recipeList").show();
 });
 
 //displayRecipe();
 function displayRecipe() {
-
   var dish = "chicken";
 
-  var queryURL = "https://api.edamam.com/search?q=" + dish + "&app_id=$385e5d34&app_key=$bf43fe764b8aae11e37d5dc0f21c1e2c&from=0&to=10&calories=0-1000000";
+  var queryURL =
+    "https://api.edamam.com/search?q=" +
+    dish +
+    "&app_id=$385e5d34&app_key=$bf43fe764b8aae11e37d5dc0f21c1e2c&from=0&to=10&calories=0-1000000";
 
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
+  }).then(function(response) {
     console.log(response);
     //console.log(queryURL);
     // storing the data from the AJAX request in the results variable
@@ -103,12 +105,9 @@ function displayRecipe() {
     console.log("hits: recipe : " + results[0].recipe.image);
     console.log("hits: recipe : " + results[0].recipe.url);
   });
-
 }
 
-
-$("#searchBtn").on("click", function () {
+$("#searchBtn").on("click", function() {
   console.log(working);
-
 });
-
+//--------------------------------How-To Video------------------------------------//
