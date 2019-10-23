29346465c7bd9b8390f62cb68c8aff5c8251d7fc
Hibah Shafi <hibahshafi.hs@gmail.com>
2019-10-22 19:35:26 -0500

    This inlcudes JS changes for recipe API, html change including div with id = recipeList, no CSS changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ let welcomePage = $(".welcome-page");
 var searchPage = $(".search-page")
 
 $('#searchBtn').hide();
+//added - Hibah 
+$("#recipeList").hide();
 
 
 $("#openingBtn").on("click", function () {
@@ -17,14 +19,47 @@ $("#openingBtn").on("click", function () {
 
 /*This on click event will handle when the video card will show, for now it is
 set to openingBtn user click*/
-$("#openingBtn").on("click", function () {
-  //This line will show the howToContainer from html
-  $("#howToContainer").show();
-});
+// $("#openingBtn").on("click", function () {
+//   //This line will show the howToContainer from html
+//   $("#howToContainer").show();
+// });
 
-var youTubeApi = AIzaSyA3LJNRXIx7_MkgahxD09FjInN0RrGgsiU;
-var youTubeUrl = "https://www.googleapis.com/youtube/v3/videos"
+// var youTubeApi = AIzaSyA3LJNRXIx7_MkgahxD09FjInN0RrGgsiU;
+// var youTubeUrl = "https://www.googleapis.com/youtube/v3/videos"
 
 
 
 //--------------------------------How-To Video------------------------------------//
+//need to add  $("#recipeList").hide(); in line 12 
+$("#openingBtn").on("click", function () {
+  //This line will show the recipelist div from html
+  $("#recipeList").show();
+});
+
+//displayRecipe();
+function displayRecipe() {
+
+  var dish = "chicken";
+
+  var queryURL = "https://api.edamam.com/search?q=" + dish + "&app_id=$385e5d34&app_key=$bf43fe764b8aae11e37d5dc0f21c1e2c&from=0&to=10&calories=0-1000000";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    //console.log(queryURL);
+    // storing the data from the AJAX request in the results variable
+    var results = response.hits;
+    console.log("hits: recipe : " + results[0].recipe.image);
+    console.log("hits: recipe : " + results[0].recipe.url);
+  });
+
+}
+
+
+$("#searchBtn").on("click", function () {
+  console.log(working);
+
+});
+
