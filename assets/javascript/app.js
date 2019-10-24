2cc3346199f7486ff5f1ff8f5e1a4a375b5bfa43
EmirVelazquez <emirleovelazquez@gmail.com>
2019-10-23 19:24:57 -0500

    logic for search bar to display youtube video, few html changes, user can click submit search or hit enter key now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 let openingPage = $("#openingPage");
 let openingBtn = $("#openingBtn");
 
-$(openingBtn).on("click", function() {
+$(openingBtn).on("click", function () {
   $(openingPage).hide();
   // $(openingPage).addClass("fadeOut");
   $(openingBtn).attr("disabled", "disabled");
@@ -16,7 +16,7 @@ var searchBtn = $("#searchBtn");
 $(searchBtn).hide();
 $(searchPage).hide();
 
-$(openingBtn).on("click", function() {
+$(openingBtn).on("click", function () {
   $(searchPage).show();
   $(searchBtn).show();
 });
@@ -25,42 +25,88 @@ $(openingBtn).on("click", function() {
 
 //--------------------------------How-To Video------------------------------------//
 
-$("#searchBtn").on("click", function() {
+//This on click event handler will call the youtube api for the video with highest rating after the user hits search button
+$("#searchBtn").on("click", function (event) {
+  //This line prevents the user from trying to submit the form, user can hit enter on keyboard or click button
+  event.preventDefault();
+  //This line makes an empty variable to hold the search
+  var userFoodSearch = [];
+  //This line will take the value from the textbox, make it lower case, trim spaces, and place inside userInput global variable
+  userInput = $("#search-input").val().toLowerCase().trim();
+  //This line will push userInput into the userFoodSearch var as a string
+  userFoodSearch.push(userInput);
+  //This line will empty the textbox so user doesn't need to delete contents after every submission
+  $("#search-input").val("");
+  //This line will clear the container holding the youTube video when user searches for a new food item
+  $("#player").empty();
+  //This local variable holds the google api key used for requesting youtube data
+  var googleApi = "AIzaSyA3LJNRXIx7_MkgahxD09FjInN0RrGgsiU";
+  //This line will call the displayYouTubeVideo function to display video searched
+  displayYouTubeVideo();
+  //This local variable holds the queryUrl used from youTube along with search parameters
+  var youTubeUrl =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=relevance&q=how+to+make+" +
+    userFoodSearch +
+    "&regionCode=us&relevanceLanguage=en&safeSearch=strict&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=any&videoEmbeddable=true&videoLicense=youtube&videoSyndicated=true&videoType=any&key=" +
+    googleApi;
+  //This starts the ajax call to request data from the youTube api  
+  $.ajax({
+    url: youTubeUrl,
+    method: "GET"
+  }).then(function (response) {
+    videoIdSearch = [];
+    videoIdSearch.push(response.items[0].id.videoId);
+    //This is for the embedded youTube video
+    //This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement("script");
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+  });
+
+});
+
+//This function will display the youTube video
+function displayYouTubeVideo() {
+  //This line will display the container holding the button and embedded youTube video
   $("#howToContainer").show();
+  //This line will change the text of the button to what the user searches for
+  $("#cookButton").text("How to make " + userInput + " video!");
+  //This line displays recipe container
   $("#recipeList").show();
-});
+}
 
-var userFoodSearch = "how to cook chicken marsala";
-var youTubeApi = "AIzaSyA3LJNRXIx7_MkgahxD09FjInN0RrGgsiU";
-var youTubeUrl =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=rating&q=" +
-  userFoodSearch +
-  "&regionCode=us&relevanceLanguage=en&safeSearch=strict&type=video&videoCaption=any&videoDefinition=any&videoDimension=2d&videoDuration=any&videoEmbeddable=true&videoLicense=youtube&videoSyndicated=true&videoType=any&key=" +
-  youTubeApi;
-$.ajax({
-  url: youTubeUrl,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+//The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
 
-//This is for the embedded video
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement("script");
+//The API calls this function when the player's state changes.
+//The function indicates that when playing a video (state=1),
+//the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    // setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
+//This function creates an <iframe> (and YouTube player)
+//after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
     height: "390",
     width: "640",
     //This id was temporarily plugged in from the response
-    videoId: "wOZFqf0ojkQ",
+    videoId: videoIdSearch,
     events: {
       onReady: onPlayerReady,
       onStateChange: onPlayerStateChange
@@ -68,30 +114,13 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
 //--------------------------------How-To Video------------------------------------//
 
 //-------------------------------- Recipes ---------------------------------------//
 
 //need to add  $("#recipeList").hide(); in line 12
-$("#openingBtn").on("click", function() {
+$("#openingBtn").on("click", function () {
   //This line will show the recipelist div from html
   $("#recipeList").show();
 });
@@ -109,7 +138,7 @@ function displayRecipe() {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(response);
     //console.log(queryURL);
     // storing the data from the AJAX request in the results variable
@@ -127,7 +156,7 @@ function displayRecipe() {
   });
 }
 
-$("#searchBtn").on("click", function() {
+$("#searchBtn").on("click", function () {
   // console.log(working);
 });
 //-------------------------------- Recipes ------------------------------------//
